525e8c97028c7551fd7b657c53af0a54c7b23025
Rajeev Chourey <rajeevchourey786@gmail.com>
2021-12-12 19:29:08 +0530

    logic

--- main.js
+++ main.js
@@ -3,7 +3,10 @@ const app = express()
 const port = process.env.PORT|| 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(json({
+      ip:req.socket.remoteAddress,
+      port:req.socket.remotePort
+  }))
 })
 
 app.listen(port, () => {
